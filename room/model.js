593664eb3e1d25e26122ce007981042c37358430
luisanegri <luisaanegri@gmail.com>
2020-01-30 13:57:33 +0100

    comment out some model fields for now

--- room/model.js
+++ room/model.js
@@ -3,44 +3,39 @@ const db = require('../db');
 
 const Room = db.define('room', {
   description: {
-    type: Sequelize.STRING,
-    allowNull: false
+    type: Sequelize.STRING
   },
   image: {
-    type: Sequelize.STRING,
-    allowNull: true
+    type: Sequelize.STRING
   },
   rent: {
-    type: Sequelize.INTEGER,
-    allowNull: false
+    type: Sequelize.INTEGER
   },
   deposit: {
-    type: Sequelize.INTEGER,
-    allowNull: false
+    type: Sequelize.INTEGER
   },
   bills: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
+    type: Sequelize.INTEGER
+  }
   //   availability: {
   //     type: Sequelize.DATE,
   //     allowNull: false
   //   },
-  couples: {
-    type: Sequelize.BOOLEAN
-  },
-  students: {
-    type: Sequelize.BOOLEAN
-  },
-  professionals: {
-    type: Sequelize.BOOLEAN
-  },
-  smoke: {
-    type: Sequelize.BOOLEAN
-  },
-  pets: {
-    type: Sequelize.BOOLEAN
-  }
+  // couples: {
+  //   type: Sequelize.BOOLEAN
+  // },
+  // students: {
+  //   type: Sequelize.BOOLEAN
+  // },
+  // professionals: {
+  //   type: Sequelize.BOOLEAN
+  // },
+  // smoke: {
+  //   type: Sequelize.BOOLEAN
+  // },
+  // pets: {
+  //   type: Sequelize.BOOLEAN
+  // }
 });
 
 module.exports = Room;
